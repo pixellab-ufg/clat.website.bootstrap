deed4047c6e7ea3cbdf859d5794c85ffeded36b4
Jaline <jalinegm@gmail.com>
2020-09-18 16:15:33 -0300

    teste tirando as cópias

---
+++ clat/js/mapa_professores.js
@@ -0,0 +1,30 @@
+import tippy from 'tippy.js';
+
+
+tippy('#sulp', {
+    content: '<strong>Sul</strong><br> 25 profissionais',
+    theme: 'custom',
+    maxWidth: 350,
+    placement: "right-start",
+});
+
+tippy('#nordestep', {
+    content: '<strong>Nordeste</strong><br> 13 profissionais',
+    theme: 'custom',
+    maxWidth: 350,
+    placement: "right-start",
+});
+
+tippy('#nortep', {
+    content: '<strong>Norte</strong><br> 7 profissionais',
+    theme: 'custom',
+    maxWidth: 350,
+    placement: "right-start",
+});
+
+tippy('#centro-oestep', {
+    content: '<strong>Centro-oeste</strong><br> 33 profissionais',
+    theme: 'custom',
+    maxWidth: 350,
+    placement: "top",
+});
